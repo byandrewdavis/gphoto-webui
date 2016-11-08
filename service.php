d9d7ad4891e92fd5c97e382d4a8e4ac912a68af8
Andrew Davis <andrew@bluewatersolutions.com>
2016-11-08 16:03:03 -0500

    Generic Updates

--- service.php
+++ service.php
@@ -24,8 +24,9 @@ try{
 	switch($action){
 
 		case "setOwner":	
-
-			exec ("gphoto2 --set-config=/main/settings/ownername=.$_POST['ownerName']).",$output);
+			$ownerName = $_GET['ownerName'];
+			echo $ownerName;
+			exec ("gphoto2 --set-config=/main/settings/ownername=.$ownerName.",$output);
 			echo json_encode(true);					
 			break;
 		case "takePicture":
